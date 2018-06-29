0788e563f8d512436a910277258c5419063a52f8
Brad Campbell <campbellb92@hotmail.com>
2018-06-29 02:43:38 -0400

    updated component ts and html for API

--- history/src/app/countries/countries.component.ts
+++ history/src/app/countries/countries.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Country } from '../country';
-import { CountryService } from '../country.service'
+import { CountryService } from '../country.service';
 
 @Component({
   selector: 'app-countries',
@@ -11,17 +13,23 @@ export class CountriesComponent implements OnInit {
 
   country: Country;
   countries: Country[]; 
+  single: Country;
 
   setCountry(c: Country){
     this.country = c;
   }
 
-  constructor( private countryService: CountryService ) { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private countryService: CountryService
+  ) {}
 
   ngOnInit() {
     this.countryService.getCountries().subscribe( // subscribing to the service
       cs => this.countries = cs
     );
+    this.countryService.getCountry(3).subscribe(data => this.single = data);
   }
 
 }
